7e570b6fe9caed2e95d3426901f9e599cd57bbae
Simon Ninon <simon.ninon@gmail.com>
2017-01-18 23:58:19 +0100

    fix migration and display listing on home page

--- migrations/20170118220947-create-primates.js
+++ migrations/20170118220947-create-primates.js
@@ -3,7 +3,7 @@ var type = dbm.dataType;
 
 exports.up = function(db, callback) {
   db.createTable('primates', {
-    id:            { type: 'int', primaryKey: true },
+    id:            { type: 'int', primaryKey: true, autoIncrement: true },
     file_name:     'string',
     recorded_date: 'date',
     species:       'string',
--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,9 @@
+//! DB fields: file_name, recorded_date, species, infant_name, time_due
 exports.view = function(req, res, db) {
-	res.render('home');
+	//! TODO: escape input
+	//! TODO: fix insert of recorded_date & time_due is broken
+	db.query('SELECT * from primates', function (error, results, fields) {
+		//! TODO: check error
+		res.render('home', { 'primates': results });
+	});
 };
