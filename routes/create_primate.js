2a30d382b1c2c6eacbc08cbe82686789b335e5c5
Mengying Li <mel116@ucsd.edu>
2017-05-14 23:01:41 -0700

    one column insert to table worked, not when add column

--- routes/create_primate.js
+++ routes/create_primate.js
@@ -3,12 +3,11 @@
 exports.view = function(req, res, db) {
 	var video_name 		= req.body.fileName;
 	var data_record     = req.body.date;
-	var species 	    = req.body.species;
 	var indivisual_name = req.body.infantName;
-    var start_time 	    = req.body.startTime;
+    var start_time 	    = data_record + ' ' + req.body.startTime;
     var activity 	= req.body.activity;
     var variation 	= req.body.variation;
-    var end_time	= req.body.endTime;
+    var end_time	= data_record + ' ' + req.body.endTime;
     var partner 	= req.body.partner;
     var comment 	= req.body.comment;
 
@@ -16,6 +15,9 @@ exports.view = function(req, res, db) {
 	//! TODO: fix insert of recorded_date & time_due is broken
 	db.query('INSERT INTO video (video_name, data_record) VALUES ("'+ video_name +'", "'+ data_record + '")', function (error, results, fields) {
 		console.log(error);
+		db.query('INSERT INTO activity (start_time, activity, variation, end_time, partner, comment) VALUES ("'+ start_time +'", "'+ activity +'", "'+ variation +'", "'+ end_time +'", "'+ partner +'", "'+ comment  + '")', function (error, results, fields) {
+			console.log(error);
+		});
 		//	db.query('INSERT INTO primates (file_name, recorded_date, species, infant_name, time_due) VALUES ("'+ file_name +'", "'+ recorded_date +'", "'+ species +'", "'+ infant_name +'", "'+ time_due +'")', function (error, results, fields){} 
         //! TODO: check error
 		res.redirect('/home');
